696c2f81ad816014a788e3a8898eb098f31462dc
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-03-18 06:46:36 +0000

    Deploying to master from @ rayadaschn/front-end-life@8db242fb2d3384179c869f13a67e77ecb12f4a32 🚀

---
+++ assets/index.html-b7e0b6fe.js
@@ -0,0 +1,1 @@
+import{_ as e,V as t,W as s,X as o}from"./framework-2060dede.js";const r={},c=o("p",null,"Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",-1),i=[c];function n(a,_){return t(),s("div",null,i)}const d=e(r,[["render",n],["__file","index.html.vue"]]);export{d as default};
