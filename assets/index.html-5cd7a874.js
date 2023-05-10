0a131e7c43ec357f584d242e8cf642347dd15ed7
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-05-10 08:43:36 +0000

    Deploying to master from @ rayadaschn/front-end-life@96745c8bd9c5e6985124ccbee9a88ce585edd682 🚀

---
+++ assets/index.html-5cd7a874.js
@@ -0,0 +1,1 @@
+import{_ as e,V as c,W as t}from"./framework-2eee3422.js";const n={},o=c,_=t;function r(l,s){return o(),_("div")}const f=e(n,[["render",r],["__file","index.html.vue"]]);export{f as default};
