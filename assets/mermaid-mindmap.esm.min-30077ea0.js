fed6c5b34e81183f723e886d5ef794dfee58590f
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2025-02-22 14:41:24 +0000

    Deploying to master from @ rayadaschn/front-end-life@a9f592f0d7d2408aaddc6972275a9ec80c884d4a 🚀

--- assets/mermaid-mindmap.esm.min-30077ea0.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-284f7b41.js";import"./framework-5dd7fabc.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
